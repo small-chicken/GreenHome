a12c6b64b6d629784c60fc772437fc1676aa94bf
Toby Willmott <tobywillmott@icloud.com>
2025-11-01 17:12:42 +0000

    user preference options

--- src/view/src/pages/Preferences.jsx
+++ src/view/src/pages/Preferences.jsx
@@ -1,12 +1,22 @@
 import Header from "../components/Header/Header.jsx";
+import PreferenceSetter from "../components/PreferenceSetter/PreferenceSetter.jsx";
 
 function Preferences () {
+    const appliances = [{name: "Washing Machine", id: 0},
+                    {name: "Dishwasher", id: 1},
+                    {name: "Dryer", id: 2},
+                    {name: "Electric Vehicle", id: 3},
+                    {name: "Heating System", id: 4},
+                    {name: "Kitchen Appliances", id: 5},
+    ]
     return (
         <div>
-        <Header />
-        <div>
-            <h1>Preferences Page</h1>
-        </div>
+            <Header />
+            <div className = "preferences-grid">
+                {appliances.map((appliance) => (
+                <PreferenceSetter appliance={appliance} key={appliance.id} />
+            ))}
+            </div>
         </div>
     )
 }
