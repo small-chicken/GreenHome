a61fd2d69f718cc3b2d4175a993236809a52f070
Dantun1 <daniel@tunyan.com>
2025-11-01 14:51:56 +0000

    Create scheduler app

--- src/backend/greenhome/settings.py
+++ src/backend/greenhome/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'greenhome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'greenhome_db',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
