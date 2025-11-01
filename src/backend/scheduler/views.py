ce1297ba48c3d6d25b9ea7effb59d1f31c70d824
small-chicken <leo.hammond@icloud.com>
2025-11-01 16:41:42 +0000

    fixed list splice error

--- src/backend/scheduler/views.py
+++ src/backend/scheduler/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
-
 from django.http import JsonResponse
-from django.views import View
 import requests
 
 class CarbonIntensityView(View):
-    def get(self):
+    def get_current_carbon(self):
         url = "https://api.carbonintensity.org.uk/intensity"
         try:
             response = requests.get(url)
             response.raise_for_status()
             data = response.json()
 
-            intensity = data["data"]["data"]["intensity"]["actual"]
-            index = data["data"]["data"]["intensity"]["index"]
+            intensity = data["data"][0]["intensity"]["actual"]
+            index = data["data"][0]["intensity"]["index"]
 
             return JsonResponse({
                 "intensity": intensity,
@@ -24,6 +22,10 @@ class CarbonIntensityView(View):
 
         except requests.RequestException as e:
             return JsonResponse({"error": str(e)}, status=500)
+        
+    def get_energy_price(self):
+        return
+
 
 
 class ScheduleView(View):
